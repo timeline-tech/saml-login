c1404cefb8d1ad7a84cee3616505dfe928c40d4c
Abhishek <abhi.sharma1114@gmail.com>
2024-09-05 13:52:45 +0530

    updating secrets

--- okta-hosted-login/azure_config.py
+++ okta-hosted-login/azure_config.py
@@ -3,11 +3,11 @@ from saml2.config import SPConfig
 
 def get_saml_azure_config():
     config = {
-        'entityid': 'http://127.0.0.1:5000',
+        'entityid': '',
         'metadata': {
             'remote': [
                 {
-                    'url': 'https://login.microsoftonline.com/ab2372e6-09e5-475a-8660-37fac8a7ef28/federationmetadata/2007-06/federationmetadata.xml?appid=cbf09074-3290-4d22-8336-318c6734b853'
+                    'url': ''
                 }
             ],
         },
@@ -15,10 +15,10 @@ def get_saml_azure_config():
             'sp': {
                 'endpoints': {
                     'assertion_consumer_service': [
-                        ('http://127.0.0.1:5000/saml/acs', BINDING_HTTP_POST),
+                        ('', BINDING_HTTP_POST),
                     ],
                     'single_logout_service': [
-                        ('https://login.microsoftonline.com/ab2372e6-09e5-475a-8660-37fac8a7ef28/federationmetadata/2007-06/federationmetadata.xml?appid=cbf09074-3290-4d22-8336-318c6734b853', BINDING_HTTP_POST),
+                        ('', BINDING_HTTP_POST),
                     ],
                 },
                 'allow_unsolicited': True,
